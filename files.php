56c24f1d64313b49d196a8b664fd0dca797f06e5
typicalname <46434915+typicalname0@users.noreply.github.com>
2020-08-09 18:03:30 -0700

    admin panel blah blah blah new stuf f blha lbhalb hbah

---
+++ files.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="/css/global.css">
+        <link rel="stylesheet" href="/css/header.css">
+        <?php
+            require("func/func.php");
+            require("func/conn.php"); 
+        ?>
+        <title>4Grounds - Hub</title>
+    </head>
+    <body> 
+        <?php require("important/header.php"); 
+        
+        if(@$_POST['submit']) {
+            if(isset($_SESSION['user'])) {
+                $stmt = $conn->prepare("INSERT INTO files (type, title, extrainfo, author, filename) VALUES ('review', ?, ?, ?, '')");
+                $stmt->bind_param("sss", $title, $description, $_SESSION['user']);
+
+                $title = htmlspecialchars($_POST['title']);
+                $description = htmlspecialchars($_POST['description']);
+                $description = str_replace(PHP_EOL, "<br>", $description);
+
+                $stmt->execute();
+                $stmt->close();
+            }
+        }
+        ?>
+        
+        <div class="container"><br>
+            <?php
+                $stmt = $conn->prepare("SELECT * FROM `files` WHERE author = ? ORDER BY id DESC");
+                $stmt->bind_param("s", $_SESSION['user']);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                while($row = $result->fetch_assoc()) { 
+                    echo "<br><img style='position: absolute;border: 1px solid white; width: 5em;' src='pfp/" . getPFP($row['author'], $conn) . "'>
+                    <small>
+                    <a href='view.php?id=" . $row['id'] . "'><span style='float:right;color: gold;'><i>[" . $row['agerating'] . "] " . $row['title'] . "</a></i></span><br>
+                    <span style='float:right;'><small><i>Posted by <a href='index.php?id=" . getID($row['author'], $conn) . "'>" . $row['author'] . "</a></i></span><br>
+                    <span style='float:right;'>" . $row['date'] . "<br>
+                    <span style='float:right;'><a href='updateinfo.php?id=" . $row['id'] . "'>Edit</a></small></small></span><br>
+                    <br><br>" . $row['extrainfo'] . "</small><hr>";
+                }
+            ?>
+        </div>
+    </body>
+</html>
---
+++ updateinfo.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="/css/global.css">
+        <link rel="stylesheet" href="/css/header.css">
+        <?php
+            require("func/func.php");
+            require("func/conn.php"); 
+        ?>
+        <title>4Grounds - Hub</title>
+    </head>
+    <body> 
+        <?php require("important/header.php"); 
+        
+        if(isset($_GET['id'])) {
+            $stmt = $conn->prepare("SELECT * FROM files WHERE author = ? AND id = ?");
+            $stmt->bind_param("si", $_SESSION['user'], $_GET['id']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            if($result->num_rows === 0) die('Item doesnt exist or item isnt made by you.');
+        } else {
+            header("Location: index.php");
+        }
+
+        if(@$_POST['submit']) {
+            if(isset($_SESSION['user'])) {
+                $stmt = $conn->prepare("UPDATE files SET title = ?, extrainfo = ? WHERE id = ?");
+                $stmt->bind_param("ssi", $title, $description, $_GET['id']);
+                $title = htmlspecialchars($_POST['title']);
+                $description = htmlspecialchars($_POST['description']);
+                $description = str_replace(PHP_EOL, "<br>", $description);
+                $stmt->execute();
+                $stmt->close();
+            } else {
+                echo "You aren't logged in.";
+            }
+        }
+        ?>
+        
+        <div class="container">
+            <h1>Edit File</h1>
+            <form method="post" enctype="multipart/form-data">
+                <input size="69" type="text" placeholder="Updated Title" name="title"><br><br>
+                <textarea required cols="81" placeholder="Updated Description" name="description"></textarea><br><br>
+                <input type="submit" value="Update Info" name="submit">
+            </form>
+        </div>
+    </body>
+</html>
