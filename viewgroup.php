0547cf154c0e8912f31eeade4176e382e0d87835
typicalname <46434915+typicalname0@users.noreply.github.com>
2020-08-11 17:21:07 -0700

    group comments

--- viewgroup.php
+++ viewgroup.php
@@ -14,6 +14,21 @@
         
         <div class="container">
             <?php
+                if($_SERVER['REQUEST_METHOD'] == 'POST') 
+                {
+                    if(!isset($_SESSION['user'])){ $error = "you are not logged in"; goto skipcomment; }
+                    if(!$_POST['comment']){ $error = "your comment cannot be blank"; goto skipcomment; }
+                    if(strlen($_POST['comment']) > 500){ $error = "your comment must be shorter than 500 characters"; goto skipcomment; }
+    
+                    $stmt = $conn->prepare("INSERT INTO `groupcomments` (toid, author, text, date) VALUES (?, ?, ?, now())");
+                    $stmt->bind_param("iss", $_GET['id'], $_SESSION['user'], $text);
+                    $unprocessedText = replaceBBcodes($_POST['comment']);
+                    $text = str_replace(PHP_EOL, "<br>", $unprocessedText);
+                    $stmt->execute();
+                    $stmt->close();
+                }
+                skipcomment:
+
                 if(isset($_GET['id'])) {
                     $stmt = $conn->prepare("SELECT * FROM `groups` WHERE id = ?");
                     $stmt->bind_param("i", $_GET['id']);
@@ -44,7 +59,36 @@
                     header("Location: viewgroups.php");
                 }
 
-            ?>
+            ?><br>
+            <hr>
+            <?php if(isset($error)) { echo "<small style='color:red'>".$error."</small>"; } ?>
+            <h2>Comment</h2>
+            <form method="post" enctype="multipart/form-data">
+                <textarea required cols="80" placeholder="Comment" name="comment"></textarea><br>
+                <input type="submit" value="Post"> <small>max limit: 500 characters | bbcode supported</small>
+            </form>
+            <hr>
+            <div id='comments'>
+            <?php
+                $stmt = $conn->prepare("SELECT * FROM `groupcomments` WHERE toid = ? ORDER BY id DESC");
+                $stmt->bind_param("s", $_GET['id']);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                
+                while($row = $result->fetch_assoc()) { ?>
+                    <div class='commentRight' style='display: grid; grid-template-columns: 75% auto; padding:5px;'>
+                        <div style="word-wrap: break-word;">
+                            <small><?php echo $row['date']; ?></small>
+                            <br>
+                            <?php echo $row['text']; ?>
+                        </div>
+                        <div>
+                            <a style='float: right;' href='?id=<?php echo getID($row['author'], $conn); ?>'><?php echo $row['author']; ?></a>
+                            <br>
+                            <img class='commentPictures' style='float: right;' height='80px;'width='80px;'src='pfp/<?php echo getPFP($row['author'], $conn); ?>'>
+                        </div>
+                    </div>
+            <?php } ?>
         </div>
     </body>
 </html>
