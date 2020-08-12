9ffbade150996c81e4dd6446481333ccbb60c3d4
ezist <maxwell1998r@gmail.com>
2020-08-11 19:02:51 -0500

    Add more detailed load average info to adminpanel

--- adminpanel/index.php
+++ adminpanel/index.php
@@ -20,7 +20,11 @@ if (function_exists("sys_getloadavg")) {
 ?>
 <h2>Server Status</h2>
 <?php
-if ($load) echo $load[0] . " Load Average<br>";
+if ($load) {
+    echo $load[0] . " load average over the last minute<br>";
+    echo $load[1] . " load average over the last 5 minutes<br>";
+    echo $load[2] . " load average over the last 15 minutes<br>";
+}
 echo memory_get_usage() . " bytes of RAM allocated to PHP";
 ?>
 <h2>Items waiting for Approval</h2>
