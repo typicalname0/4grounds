233244e20a24ead3c61c9779fa250d97fc20f687
ezist <maxwell1998r@gmail.com>
2020-08-13 00:10:08 -0500

    Replace BBcode with Markdown in profile bios (#8)

--- index.php
+++ index.php
@@ -173,7 +173,7 @@
                     </div><br>
                     <div id="bio" class="notegray">
                         <h1>Bio</h1>
-                        <?php echo str_replace(PHP_EOL, "<br>", replaceBBcodes($bio)); ?>
+                        <?php echo Michelf\Markdown::defaultTransform($bio); ?>
                     </div><br><br>
                     <div id='comments'>
                         <?php
