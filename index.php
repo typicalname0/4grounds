cd83d8cc3ef118a7f0a30e169f147a1cd17d2659
bloxxite <rflorintsev@gmail.com>
2020-08-16 10:41:50 -0500

    correct image src

--- index.php
+++ index.php
@@ -281,7 +281,7 @@
                     $result = $stmt->get_result();
                     while($row = $result->fetch_assoc()) {
                         echo "<div style='display: inline-block;' class='notegray'>
-                            <a href='/view?id=" . $row['id'] . "'><img style='width: 7.5em;height: 7.5em;' src='/images/" . $row['filename'] . "'>
+                            <a href='/view?id=" . $row['id'] . "'><img style='width: 7.5em;height: 7.5em;' src='/dynamic/image/" . $row['filename'] . "'>
                             <br><center><b>" . htmlspecialchars($row['title']) . "</b><br><span style='color: gray;'>By " . $row['author'] . "</span></center>
                             </a>
                         </div> ";  
