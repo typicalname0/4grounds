69d941e3f58edcf85d3bf00bf1be0199f133820a
ezist <maxwell1998r@gmail.com>
2020-08-17 17:00:02 -0500

    Fix comments in defaults.inc.php and fix up both
    Signed-off-by: ezist <maxwell1998r@gmail.com>

--- cfg/config.inc.php
+++ cfg/config.inc.php
@@ -8,10 +8,10 @@
 //
 //   4Grounds configuration file. See default values and all options in defaults.inc.php
 //   Uncomment values by removing the // before changing!
-//
+
     $config = array();
 
-    include("defaults.inc.php");
+    include(__DIR__ . "/defaults.inc.php");
 
     // $config['database_host'] = 'localhost';
     // $config['database_database'] = 'fourground';
@@ -21,4 +21,3 @@
     // $config['use_recaptcha'] = false;
     // $config['recaptcha_secret'] = '';
     // $config['recaptcha_sitekey'] = '';    
-?>
