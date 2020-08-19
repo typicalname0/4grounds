c257a41ce69f8de3351a8a92742f5ddbda566b34
derekchauvin0 <69917782+derekchauvin0@users.noreply.github.com>
2020-08-19 13:36:02 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -7,8 +7,8 @@
 	 <body>
 	     <?php require(__DIR__ . "/important/header.php"); ?>
 		  <div class="container" style="text-align: center;">
-		      <h1>404 Not Found</h1>
-				The page you were trying to access could not be found.<br><br>
+		      <h1>404 Slavery Not Found</h1>
+				The plantation you were trying to access could not be found.<br><br>
 				<a href="##" onclick="history.go(-1); return false;">&lt;&lt; back</a>
 		  </div>
 	 </body>
