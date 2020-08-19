2e090ea13706855970bae5588829370fe3a8fcbd
typicalname <46434915+typicalname0@users.noreply.github.com>
2020-08-19 12:55:46 -0700

    i think this will work

--- view/index.php
+++ view/index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="../css/global.css">
-        <link rel="stylesheet" href="../css/header.css">
+        <link rel="stylesheet" href="/static/css/global.css">
+        <link rel="stylesheet" href="/static/css/header.css">
+		<link rel="stylesheet" href="/static/css/index.css">
         <?php
-            require(__DIR__ . "/../func/func.php");
-            require(__DIR__ . "/../func/conn.php"); 
+            require(__DIR__ . "/func/func.php");
+            require(__DIR__ . "/func/conn.php"); 
 
             if(isset($_GET['id'])) {
                 $stmt = $conn->prepare("SELECT * FROM files WHERE id = ?");
