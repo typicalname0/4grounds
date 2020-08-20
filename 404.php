f824321df1c2f233608ebfb319b1d91d9772c3ac
ezist <maxwell1998r@gmail.com>
2020-08-19 19:09:21 -0500

    Indentation
    Signed-off-by: ezist <maxwell1998r@gmail.com>

--- 404.php
+++ 404.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html>
-    <head>
-	     <link rel="stylesheet" href="/static/css/global.css" />
-		  <link rel="stylesheet" href="/static/css/header.css" />
-	 </head>
-	 <body>
-	     <?php require(__DIR__ . "/important/header.php"); ?>
-		  <div class="container" style="text-align: center;">
-		      <h1>404 Not Found</h1>
-				The page you were trying to access could not be found.<br><br>
-				<a href="##" onclick="history.go(-1); return false;">&lt;&lt; back</a>
-		  </div>
-	 </body>
+     <head>
+        <link rel="stylesheet" href="/static/css/global.css" />
+        <link rel="stylesheet" href="/static/css/header.css" />
+    </head>
+    <body>
+        <?php require(__DIR__ . "/important/header.php"); ?>
+        <div class="container" style="text-align: center;">
+            <h1>404 Not Found</h1>
+            The page you were trying to access could not be found.<br><br>
+            <a href="##" onclick="history.go(-1); return false;">&lt;&lt; back</a>
+        </div>
+    </body>
 </html>
