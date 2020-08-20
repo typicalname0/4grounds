7d489b3c0510e396bb3969b5603f7db62b9c1e61
Niggersblack42 <69942212+Niggersblack42@users.noreply.github.com>
2020-08-19 20:07:52 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -7,8 +7,8 @@
 	 <body>
 	     <?php require(__DIR__ . "/important/header.php"); ?>
 		  <div class="container" style="text-align: center;">
-		      <h1>404 Not Found</h1>
-				The page you were trying to access could not be found.<br><br>
+		      <h1>404 Nigger Found</h1>
+				The page you were trying to access had niggers found.<br><br>
 				<a href="##" onclick="history.go(-1); return false;">&lt;&lt; back</a>
 		  </div>
 	 </body>
